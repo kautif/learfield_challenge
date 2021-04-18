bcb999639d70bb3e6fc2a2ec935e46d76fa535e1
Autif Kamal <kautif@gmail.com>
2021-04-18 17:14:45 -0600

    added comments for teams dropdown

--- menu.js
+++ menu.js
@@ -3,23 +3,24 @@ let teamsDropdown = document.getElementsByClassName('teams-dropdown')[0];
 let mobileNavIcon = document.getElementsByClassName('mobile-nav-icon')[0];
 let mobileNav = document.getElementsByClassName('mobile-nav')[0];
 
-// Show Teams Dropdown
 teams[0].onmouseover = function () {
     teams[0].classList.add('main-nav-item--selected');
     document.getElementsByClassName('teams-dropdown')[0].classList.add('teams-dropdown--show');
 }
-// Hide Teams Dropdown
+
 function removeDropdown () {
     teams[0].classList.remove('main-nav-item--selected');
     teamsDropdown.classList.remove('teams-dropdown--show');
 }
 
 // Tickets
+    //  If user hovers over Tickets, dropdown is removed
+    //  If user hovers back into the Teams Dropdown and back into another nav item,
+    //      dropdown is removed
 teams[1].onmouseover = function () {
     removeDropdown();
 }
 
-// Mouse exits from Teams Dropdown
 teamsDropdown.onmouseleave = function () {
     removeDropdown();
 }
