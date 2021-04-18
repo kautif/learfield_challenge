5fe838bdc3eb2d85767c3ca257f2552ea2643bb7
Autif Kamal <kautif@gmail.com>
2021-04-18 13:32:04 -0600

    updated class nomenclature and heirarchy

--- menu.js
+++ menu.js
@@ -1,21 +1,21 @@
-let teams = document.getElementsByClassName('main-nav-item');
+let teams = document.getElementsByClassName('main-nav__item');
 let teamsDropdown = document.getElementsByClassName('teams-dropdown')[0];
 let mobileNavIcon = document.getElementsByClassName('mobile-nav-icon')[0];
 let mobileNav = document.getElementsByClassName('mobile-nav')[0];
 
 teams[0].onmouseover = function () {
-    teams[0].classList.add('main-nav-item-selected');
-    document.getElementsByClassName('teams-dropdown')[0].classList.add('teams-dropdown-show');
+    teams[0].classList.add('main-nav-item--selected');
+    document.getElementsByClassName('teams-dropdown')[0].classList.add('teams-dropdown--show');
 }
 
 teams[1].onmouseover = function () {
-    teams[0].classList.remove('main-nav-item-selected');
-    teamsDropdown.classList.remove('teams-dropdown-show');
+    teams[0].classList.remove('main-nav-item--selected');
+    teamsDropdown.classList.remove('teams-dropdown--show');
 }
 
 teamsDropdown.onmouseleave = function () {
-    teams[0].classList.remove('main-nav-item-selected');
-    teamsDropdown.classList.remove('teams-dropdown-show');
+    teams[0].classList.remove('main-nav-item--selected');
+    teamsDropdown.classList.remove('teams-dropdown--show');
 }
 
 $(document).ready(function () {
@@ -23,12 +23,6 @@ $(document).ready(function () {
     if (document.body.clientWidth < 1024) {
         document.getElementsByClassName('mobile-nav-container')[0].style.height = $('body').height() - $('.main-nav').height() + 'px'; 
     }
-
-    // $('.mobile-nav-icon').on('click', function() {
-    //     $('.mobile-nav-container').slideToggle('slow');
-    //     $('.fa-bars').fadeToggle('fast', 'linear');
-    //     $('.fa-times').fadeToggle('fast', 'linear');
-    // });
 
     let isMobileMenuOpen = false;
     $('.fa-bars').on('click', function () {
@@ -56,7 +50,7 @@ $(document).ready(function () {
     let isTeamsCollapsed = false;
     $('.fa-chevron-down').on('click', function () {
         if (!isTeamsCollapsed) {
-            $('.mobile-nav-teams').slideToggle('slow');
+            $('.mobile-nav__teams').slideToggle('slow');
             $('.fa-chevron-down').fadeOut();
             setTimeout(function () {
                 $('.fa-chevron-up').fadeIn();
@@ -67,7 +61,7 @@ $(document).ready(function () {
 
     $('.fa-chevron-up').on('click', function () {
         if (isTeamsCollapsed) {
-            $('.mobile-nav-teams').slideToggle('slow');
+            $('.mobile-nav__teams').slideToggle('slow');
             $('.fa-chevron-up').fadeOut();
             setTimeout(function () {
                 $('.fa-chevron-down').fadeIn();
