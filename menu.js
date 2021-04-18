01442c73031a04ea7f881e45a04130d60fe358dc
Autif Kamal <kautif@gmail.com>
2021-04-18 16:44:56 -0600

    added comments for js files

--- menu.js
+++ menu.js
@@ -3,20 +3,23 @@ let teamsDropdown = document.getElementsByClassName('teams-dropdown')[0];
 let mobileNavIcon = document.getElementsByClassName('mobile-nav-icon')[0];
 let mobileNav = document.getElementsByClassName('mobile-nav')[0];
 
+// Show Teams Dropdown
 teams[0].onmouseover = function () {
     teams[0].classList.add('main-nav-item--selected');
     document.getElementsByClassName('teams-dropdown')[0].classList.add('teams-dropdown--show');
 }
-
+// Hide Teams Dropdown
 function removeDropdown () {
     teams[0].classList.remove('main-nav-item--selected');
     teamsDropdown.classList.remove('teams-dropdown--show');
 }
 
+// Tickets
 teams[1].onmouseover = function () {
     removeDropdown();
 }
 
+// Mouse exits from Teams Dropdown
 teamsDropdown.onmouseleave = function () {
     removeDropdown();
 }
--- mobile-menu.js
+++ mobile-menu.js
@@ -1,9 +1,12 @@
 $(document).ready(function () {
     $('.fa-chevron-up').hide();
+    // If screensize is less than 1024px, body is equal to 100vh 
+    // and mobile menu is equal to body minus height of main nav section in px
     if (document.body.clientWidth < 1024) {
         document.getElementsByClassName('mobile-nav-container')[0].style.height = $('body').height() - $('.main-nav').height() + 'px'; 
     }
 
+    // Show or hide selected menu as well as icons associated with opening or closing
     function handleMobileMenu (mobileMenu, iconExit, iconEnter) {
         $(mobileMenu).slideToggle('slow');
         $(iconExit).fadeOut();
